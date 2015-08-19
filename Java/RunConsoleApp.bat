c7d66a8ae9f265bae69c0cc1821cbf226565ed27
Joe Wright <joe@joejag.com>
2015-08-19 22:04:28 +0100

    drop first version

--- Java/RunConsoleApp.bat
+++
@@ -1,8 +0,0 @@
-cls
-# REM delete build output
-rmdir /s /q bin
-mkdir bin\codecraft\ticketbooker
-
-REM Build and Run App
-javac -cp src\main\java; src\main\java\codecraft\ticketbooker\*.java -d bin\codecraft\ticketbooker
-java -cp bin\codecraft\ticketbooker codecraft.ticketbooker.ConsoleApp
