6e35f88e6325a93e6f15a1d0e504fb18c1943bcb
keith <keithharrisonuk@yahoo.co.uk>
2015-08-27 09:36:51 +0100

    Changes to pass linting.

--- nodejs/ticketbooker/lib/tickets.js
+++ nodejs/ticketbooker/lib/tickets.js
@@ -12,8 +12,8 @@ function Tickets() {
 method.getReport = function () {
   var standardTicketTotal = 0.0;
   var studentTicketTotal = 0.0;
-  for (var i = 0; i < this.ticketArray.length; i++) {
-    var t1 = this.ticketArray[i];
+  for (var i1 = 0; i1 < this.ticketArray.length; i1++) {
+    var t1 = this.ticketArray[i1];
     switch (t1.CustomerType) {
       case Ticket.CustomerTypes.Standard:
         standardTicketTotal += this.ticketPrice;
@@ -21,32 +21,32 @@ method.getReport = function () {
       case Ticket.CustomerTypes.Student:
         studentTicketTotal += this.ticketPrice * 0.6;
         break;
-      }
+    }
   }
   var total = standardTicketTotal + studentTicketTotal;
 
   var reportOutput = '** SALES REPORT **' + newLine + newLine;
   reportOutput += 'Number of Tickets Sold' + newLine;
   var standardTicketCount = 0;
-  for (var i = 0; i < this.ticketArray.length; i++) {
-    var t2 = this.ticketArray[i];
+  for (var i2 = 0; i2 < this.ticketArray.length; i2++) {
+    var t2 = this.ticketArray[i2];
     if (t2.CustomerType === Ticket.CustomerTypes.Standard) {
       standardTicketCount++;
     }
   }
   reportOutput += '  Standard: ' + standardTicketCount + newLine;
   var studentTicketCount = 0;
-  for (var i = 0; i < this.ticketArray.length; i++) {
-    var t3 = this.ticketArray[i];
+  for (var i3 = 0; i3 < this.ticketArray.length; i3++) {
+    var t3 = this.ticketArray[i3];
     if (t3.CustomerType === Ticket.CustomerTypes.Student) {
       studentTicketCount++;
     }
   }
   reportOutput += '  Student: ' + studentTicketCount + newLine + newLine;
-  reportOutput += "Income" + newLine;
-  reportOutput += "  Standard Total = £" + standardTicketTotal + newLine;
-  reportOutput += "  Student Total = £" + studentTicketTotal + newLine;
-  reportOutput += "  Total = £" + total;
+  reportOutput += 'Income' + newLine;
+  reportOutput += '  Standard Total = £' + standardTicketTotal + newLine;
+  reportOutput += '  Student Total = £' + studentTicketTotal + newLine;
+  reportOutput += '  Total = £' + total;
   return reportOutput;
 };
 
